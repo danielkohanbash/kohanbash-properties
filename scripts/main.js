9c5ac7f7c6cc27848df3df36345d93201db74873
Nikkie Mashian <nikkiemashian@gmail.com>
2018-11-22 08:20:22 -0800

    navbar css done

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,31 @@
 'use strict';
 
-var stickySidebar = $('.navbar-fixed-top').offset().top;
-$(window).scroll(function() {  
-  if ($(window).scrollTop() > stickySidebar) {
-    $('.navbar-fixed-top').addClass('affix');
-    $('.dontshow').addClass('showit');
-    $('.navbar').css('border-bottom' , '3px solid rgb(209,164,35)');
-  }
-  else {
-    $('.navbar-fixed-top').removeClass('affix');
-    $('.dontshow').removeClass('showit');
-    $('.navbar').css('border-bottom' , 'none'); 
-  }  
+function adjustNavbarOnScroll(){
+  let stickySidebar = $('.navbar-fixed-top').offset().top;
+  $(window).scroll(function() {  
+    if ($(window).scrollTop() > stickySidebar) {
+      $('.navbar-fixed-top').addClass('affix');
+      $('.dontshow').addClass('showit');
+    }
+    else {
+      $('.navbar-fixed-top').removeClass('affix');
+      $('.dontshow').removeClass('showit');
+    }  
+  });
+}
+
+function handleClickOutsideNavbar(){
+  $(document).click(function (event) {
+    var clickover = $(event.target);
+    var $navbar = $('.navbar-collapse');               
+    var _opened = $navbar.hasClass('in');
+    if (_opened === true && !clickover.hasClass('navbar-toggle')) {      
+      $navbar.collapse('hide');
+    }
+  }); //this is so that clicking outside of navbar closes/collapses navbar/collapses (doesn't work on mobile)
+}
+
+$(document).ready(function(){
+  adjustNavbarOnScroll();
+  handleClickOutsideNavbar();
 });
